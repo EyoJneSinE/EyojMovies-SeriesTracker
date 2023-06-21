c3f3611de0f0ec165fe6d21f4a45d1b45556e0e4
Enis <enis.d.kaner@gmail.com>
2023-06-21 19:50:02 +0300

    MovieApplication added for dependency injection with Hilt

---
+++ app/src/main/java/com/eniskaner/eyojmovietrackerwithcompose/MovieApplication.kt
@@ -0,0 +1,8 @@
+package com.eniskaner.eyojmovietrackerwithcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MovieApplication: Application() {
+}
