273e4b2172c2998085bdd8841af60f2625da8937
Enis <enis.d.kaner@gmail.com>
2023-06-21 19:11:24 +0300

    MovieDetail domain model added

---
+++ app/src/main/java/com/eniskaner/eyojmovietrackerwithcompose/domain/model/MovieDetail.kt
@@ -0,0 +1,20 @@
+package com.eniskaner.eyojmovietrackerwithcompose.domain.model
+
+data class MovieDetail(
+    val Actors: String,
+    val Awards: String,
+    val BoxOffice: String,
+    val Country: String,
+    val Director: String,
+    val Genre: String,
+    val Language: String,
+    val Plot: String,
+    val Poster: String,
+    val Rated: String,
+    val Released: String,
+    val Runtime: String,
+    val Title: String,
+    val Type: String,
+    val Year: String,
+    val imdbRating: String,
+)
