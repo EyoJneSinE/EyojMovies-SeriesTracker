663ed4cbad1c6263075955f519f620c1ea7d962e
Enis <enis.d.kaner@gmail.com>
2023-06-21 19:00:28 +0300

    map func added for MoviesDTO to Movie

--- app/src/main/java/com/eniskaner/eyojmovietrackerwithcompose/data/remote/dto/MoviesDTO.kt
+++ app/src/main/java/com/eniskaner/eyojmovietrackerwithcompose/data/remote/dto/MoviesDTO.kt
@@ -1,7 +1,15 @@
 package com.eniskaner.eyojmovietrackerwithcompose.data.remote.dto
 
+import com.eniskaner.eyojmovietrackerwithcompose.domain.model.Movie
+
 data class MoviesDTO(
     val Response: String,
     val Search: List<Search>,
     val totalResults: String
 )
+
+fun MoviesDTO.toMovieList(): List<Movie> {
+    return Search.map { search ->
+        Movie(search.Poster, search.Title, search.imdbID, search.Year)
+    }
+}
