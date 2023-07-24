d4811fe98c88c0fdaf7f9eeb5e7f294e11ac2b95
Enis <enis.d.kaner@gmail.com>
2023-07-24 14:02:21 +0300

    MoviesViewModel contents added for MoviesScreen

--- app/src/main/java/com/eniskaner/eyojmovietrackerwithcompose/presentation/movies/MoviesViewModel.kt
+++ app/src/main/java/com/eniskaner/eyojmovietrackerwithcompose/presentation/movies/MoviesViewModel.kt
@@ -1,10 +1,54 @@
 package com.eniskaner.eyojmovietrackerwithcompose.presentation.movies
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.eniskaner.eyojmovietrackerwithcompose.domain.usecase.GetMoviesUseCase
+import com.eniskaner.eyojmovietrackerwithcompose.util.Resource
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 class MoviesViewModel @Inject constructor(
     private val getMoviesUseCase: GetMoviesUseCase
 ) : ViewModel() {
+
+    private val _state = mutableStateOf<MoviesState>(MoviesState())
+    val state : State<MoviesState> = _state
+
+    private var job : Job? = null
+
+    init {
+        getMovies(_state.value.search)
+    }
+
+    private fun getMovies(search: String) {
+        job?.cancel()
+        job = getMoviesUseCase.executeGetMovies(search).onEach {
+            when (it) {
+                is Resource.Success -> {
+                    _state.value = MoviesState(movies = it.data ?: emptyList())
+                }
+
+                is Resource.Error -> {
+                    _state.value = MoviesState(error = it.message ?: "Error!")
+                }
+
+                is Resource.Loading -> {
+                    _state.value = MoviesState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    fun onEvent(event : MoviesEvent) {
+        when(event) {
+            is MoviesEvent.Search -> {
+                getMovies(event.searchString)
+            }
+        }
+    }
+
 }
