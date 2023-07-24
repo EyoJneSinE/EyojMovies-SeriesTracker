fcb2414637ecee8c22e568aeb94bba5c776f6a59
Enis <enis.d.kaner@gmail.com>
2023-07-24 15:17:44 +0300

    MovieScreen added and contents added for check display issues

---
+++ app/src/main/java/com/eniskaner/eyojmovietrackerwithcompose/presentation/movies/views/MovieScreen.kt
@@ -0,0 +1,108 @@
+package com.eniskaner.eyojmovietrackerwithcompose.presentation.movies.views
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.*
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.eniskaner.eyojmovietrackerwithcompose.presentation.movies.MoviesEvent
+import com.eniskaner.eyojmovietrackerwithcompose.presentation.movies.MoviesViewModel
+
+@Composable
+fun MovieScreen(
+    navController: NavController,
+    viewModel : MoviesViewModel = hiltViewModel()
+) {
+
+    val state = viewModel.state.value
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Black)) {
+
+        Column() {
+
+            MovieSearchBar(modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+                hint = "marvel",
+                onSearch = {
+                    viewModel.onEvent(MoviesEvent.Search(it))
+                }
+            )
+
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(state.movies) {movie ->
+                    Text(text = movie.Title, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center, color = Color.White)
+
+
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieSearchBar(
+    modifier : Modifier,
+    hint: String = "",
+    onSearch: (String) -> Unit = {}
+) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    var isHintDisplayed by remember {
+        mutableStateOf(hint != "")
+    }
+
+    Box(modifier = modifier) {
+        BasicTextField(value = text,
+            keyboardActions = KeyboardActions(onDone = {
+                onSearch(text)
+            }),
+            onValueChange = {
+                text = it
+            },
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(5.dp, CircleShape)
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp)
+                .onFocusChanged {
+                    isHintDisplayed = it.isFocused != true && text.isEmpty()
+                }
+        )
+        if (isHintDisplayed) {
+            Text(
+                text = hint,
+                color = Color.LightGray,
+                modifier = Modifier
+                    .padding(horizontal = 20.dp, vertical = 12.dp)
+            )
+        }
+    }
+}
