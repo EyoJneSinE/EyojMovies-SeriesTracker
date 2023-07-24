f967a4e0a9cc9791199e47262935461e97305f16
Enis <enis.d.kaner@gmail.com>
2023-07-24 14:15:37 +0300

    Internet permission added on AndroidManifest

--- app/src/main/java/com/eniskaner/eyojmovietrackerwithcompose/presentation/movies/MoviesViewModel.kt
+++ app/src/main/java/com/eniskaner/eyojmovietrackerwithcompose/presentation/movies/MoviesViewModel.kt
@@ -6,11 +6,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.eniskaner.eyojmovietrackerwithcompose.domain.usecase.GetMoviesUseCase
 import com.eniskaner.eyojmovietrackerwithcompose.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
+@HiltViewModel
 class MoviesViewModel @Inject constructor(
     private val getMoviesUseCase: GetMoviesUseCase
 ) : ViewModel() {
